6835af12afe9b5166e63d8cfba3a5b3345595bcf
Bahir Hussain <mbahirh624@gmail.com>
2024-08-19 15:23:34 +0530

    Update Header Code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
-import UseStateFunction from './components/ReactHooks/UseStateFunction.jsx'; // Correct casing
+// import UseStateFunction from './components/ReactHooks/UseStateFunction.jsx'; // Correct casing
 import Footer from './components/Footer.jsx';
 import Header from './components/Header';
-import UseStateFunctonDouble from './components/ReactHooks/UseStateFunctonDouble.jsx';
-import UseEffectFunction from './components/ReactHooks/UseEffectFunction.jsx';
+// import UseStateFunctonDouble from './components/ReactHooks/UseStateFunctonDouble.jsx';
+// import UseEffectFunction from './components/ReactHooks/UseEffectFunction.jsx';
+import Cartbutton from "./components/Cartbutton.js"
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <UseStateFunction/>
-      <UseStateFunctonDouble/>
-      <UseEffectFunction/>
+      <Cartbutton/>
       <Footer/>
     </div>
   );
--- src/components/Cartbutton.js
+++ src/components/Cartbutton.js
@@ -8,7 +8,7 @@ const Cartbutton = () => {
         <Card border="primary" style={{ width: "18rem" }}>
           <Card.Header>Header</Card.Header>
           <Card.Body>
-            <Card.Title>Primary Card Title</Card.Title>
+            <Card.Title>Header Code</Card.Title>
             <Card.Text>
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
