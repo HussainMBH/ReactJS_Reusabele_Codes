ce45e7501baf378270a63fa4485c1288aa7400ff
Bahir Hussain <mbahirh624@gmail.com>
2024-10-02 11:53:25 +0530

    Update UseRef

--- src/components/ReactHooks/UseRefFunction.jsx
+++ src/components/ReactHooks/UseRefFunction.jsx
@@ -1,19 +1,23 @@
-import React, {useState} from 'react'
+import React, { useRef, useState } from 'react';
 
 function UseRefFunction() {
-    const [input, setInput] = useState("");
+  const inputRef = useRef();
+  const [inputValue, setInputValue] = useState('');
+
+  const handleClick = () => {
+    const value = inputRef.current.value;
+    //alert(`You entered: ${value}`);
+    setInputValue(value); // Update the state with the input value
+  };
 
   return (
     <div>
       <h1>Input</h1>
-      <input
-      type='text'
-      value={input}
-      onChange={(event) => setInput(event.target.value)}
-      />
-      <p>My name is {input}</p>
+      <input type="text" ref={inputRef} placeholder="Type something..." />
+      <button onClick={handleClick}>Submit</button>
+      <p>Your Entered: {inputValue}</p>
     </div>
-  )
+  );
 }
 
-export default UseRefFunction
+export default UseRefFunction;
